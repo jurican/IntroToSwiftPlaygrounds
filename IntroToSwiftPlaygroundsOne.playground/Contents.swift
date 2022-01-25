ad56b2a1da1e77f65308fa21e52a479c95700ef7
jurican <97710250+jurican@users.noreply.github.com>
2022-01-24 20:46:03 -1000

    Removed Hello World and cleaned up Var statements

--- IntroToSwiftPlaygroundsOne.playground/Contents.swift
+++ IntroToSwiftPlaygroundsOne.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-var greeting = "Hello, playground"
-var myFirstVariable = "I've created my first variable"
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
 var mySecondVariable = " and look I added 2 strings together"
 var myThirdVariable = myFirstVariable + mySecondVariable
 print (myThirdVariable)
