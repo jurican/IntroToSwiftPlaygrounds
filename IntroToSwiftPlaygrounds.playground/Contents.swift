9289bf2a0a9112986949a8e5ee8f3aa603fc7472
jurican <97710250+jurican@users.noreply.github.com>
2022-01-24 20:11:55 -1000

    Completed Swift Playgrounds #1

--- IntroToSwiftPlaygrounds.playground/Contents.swift
+++ IntroToSwiftPlaygrounds.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
 var greeting = "Hello, playground"
+var myFirstVariable = "I've created my first variable"
+var mySecondVariable = " and look I added 2 strings together"
+var myThirdVariable = myFirstVariable + mySecondVariable
+print (myThirdVariable)
+var afCoreValue1 = "Integrity First"
+var afCoreValue2 = ", Service Before Self"
+var afCoreValue3 = ", Excellence in All We Do!"
+var AFCOREVALUES = afCoreValue1 + afCoreValue2 + afCoreValue3
+print (AFCOREVALUES)
