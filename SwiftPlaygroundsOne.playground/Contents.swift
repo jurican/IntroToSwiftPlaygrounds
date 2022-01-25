e963a992b3b975aa023c4c4ab38b4243de78ffe9
jurican <97710250+jurican@users.noreply.github.com>
2022-01-24 21:27:12 -1000

    “Equal to” Operator

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -2,11 +2,30 @@ import UIKit
 
 var myFirstVariable: String
 myFirstVariable = "I've created my first variable"
+
+//We are gonna do something with strings
 var mySecondVariable = " and look I added 2 strings together"
 var myThirdVariable = myFirstVariable + mySecondVariable
 print (myThirdVariable)
 var afCoreValue1 = "Integrity First"
 var afCoreValue2 = ", Service Before Self"
 var afCoreValue3 = ", Excellence in All We Do!"
-var AFCOREVALUES = afCoreValue1 + afCoreValue2 + afCoreValue3
-print (AFCOREVALUES)
+var afCoreValues = afCoreValue1 + afCoreValue2 + afCoreValue3
+print (afCoreValues)
+
+// Assignment #2 Introduction to True or False
+var appState = false
+print(appState)
+
+//Comparing variables using > (greater than) or < (less than)
+var numberOne = 5
+var numberTwo = 7
+
+//Create a variable that holds the comparison of the two variables we just created
+var compare = numberOne > numberTwo
+
+//Equal to Operator
+var equalToo = 2
+var equalTwo = 2
+var equal2 = equalToo == equalTwo
+print(equal2)
